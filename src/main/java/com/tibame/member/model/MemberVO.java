101fb31eaa3d94dbdde3037b3d0a3414e4442e81
K.H.Wu <kuw294523@gmail.com>
2025-05-29 15:12:48 +0800

    新增MemberVO類別

---
+++ src/main/java/com/tibame/member/model/MemberVO.java
@@ -0,0 +1,28 @@
+package com.tibame.member.model;
+
+public class MemberVO {
+	private Integer memId;
+	private String memName;
+	
+	public MemberVO() {
+		super();
+	}
+	public MemberVO(Integer memId, String memName) {
+		super();
+		this.memId = memId;
+		this.memName = memName;
+	}
+	public Integer getMemId() {
+		return memId;
+	}
+	public void setMemId(Integer memId) {
+		this.memId = memId;
+	}
+	public String getMemName() {
+		return memName;
+	}
+	public void setMemName(String memName) {
+		this.memName = memName;
+	}
+	
+}
